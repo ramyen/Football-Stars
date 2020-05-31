2e2e030d6c37774868b478ee68cf6d245d12fd52
ramyen <ramyen@gmail.com>
2020-05-30 22:24:26 -0700

    - add stuffs for test

---
+++ Client/Football-Stars-Client/Assets/Script/Runtime/Scene/PlayScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayScene : MonoBehaviour
+{
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        Debug.Log("test");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
